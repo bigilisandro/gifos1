1572be60f3addf0a2e30ff4d6ec345257ea3679d
Lisandro Bigi <55090798+bigilisandro@users.noreply.github.com>
2020-04-03 19:15:59 -0300

    lslas

--- script/upload.js
+++ script/upload.js
@@ -1,6 +1,7 @@
+
+// JAVASCRIPT
+
 // Cambiar tema light - dark mode 
-
-
 
 function day(){
     body.className='day';
@@ -11,4 +12,18 @@ function day(){
     body.className='night';
     logo.src='assets/gifOF_logo_dark.png';
   }
+
+  // FUNCIÓN VÍDEO
   
+  function getStreamAndRecord () {
+    navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+    height: { max: 480 }
+    }
+    })
+    .then(function(stream) {
+    video.srcObject = stream;
+    video.play()
+    }
+    
