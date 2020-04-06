d09deeaf8e146d21a4db7df2aa5df6a72f8aa7aa
Lisandro Bigi <55090798+bigilisandro@users.noreply.github.com>
2020-04-06 18:35:33 -0300

    fbdg

--- script/upload.js
+++ script/upload.js
@@ -15,59 +15,87 @@ function night(){
 
   // FUNCIÓN VÍDEO
 
-  // Obtener vídeo
-
   function getStreamAndRecord() {
-    navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-    height: { max: 480 }
+      navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+      height: { max: 480 }
+      }
+      })
+      .then(function(camera) {
+      video.srcObject = camera;
+      video.play()
+      }
+      )}
+
+
+
+  
+    /* function captureCamera(callback) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function(camera) {
+            callback(camera);
+        }).catch(function(error) {
+            console.error(error);
+        });
     }
-    })
-    .then(function(stream) {
-      console.log(video);
-    video.srcObject = stream;
-    video.play()
+    */
+    
+    function stopRecordingCallback() {
+        video.srcObject = URL.createObjectURL(recorder.getBlob());
+        recorder.camera.stop();
+        recorder.destroy();
+        recorder = null;
     }
-    )
-  }
-
-  // Objeto Recorder
-
-  recorder = RecordRTC(stream, {
-    type: 'gif',
-    frameRate: 1,
-    quality: 10,
-    width: 360,
-    hidden: 240,
-    onGifRecordingStarted: function() {
-    console.log('started')
-    },
-    });
-
-  // Grabar / Detener grabación
-
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(async function(stream) {
-    let recorder = RecordRTC(stream, {
-        type: 'video'
-    });
-    recorder.startRecording();
-
-    const sleep = m => new Promise(r => setTimeout(r, m));
-    await sleep(3000);
-
-    recorder.stopRecording(function() {
-        let blob = recorder.getBlob();
-        invokeSaveAsDialog(blob);
-    });
-});
-
-
+    
+    var recorder; // globally accessible
+    
+    function getStreamAndRecord2() {
+        this.disabled = true;
+        getStreamAndRecord(function(camera) {
+            document.querySelector('h1').innerHTML = 'Waiting for Gif Recorder to start...';
+            recorder = RecordRTC(camera, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 360,
+                hidden: 240,
+                onGifRecordingStarted: function() {
+                    document.querySelector('h1').innerHTML = 'Gif recording started.';
+                },
+                onGifPreview: function(gifURL) {
+                    image.src = gifURL;
+                },
+              
+            });
+            
+          
+    
+            recorder.startRecording();
+        
+            // release camera on stopRecording
+            recorder.camera = camera;
+    
+            document.getElementById('btn-stop-recording').disabled = false;
+        });
+    };
+    
+    function stopRecord() {
+        this.disabled = true;
+        recorder.stopRecording(stopRecordingCallback);
+    };
 /*
   let form = new FormData();
   form.append('file', recorder.getBlob(), 'myGif.gif');
   console.log(form.get('file'));
 */
+
+// Cambios en el DOM al presionar botones
+
+function startVideo() {
+  document.getElementById("recuadro").style.display = "none";
+  document.getElementById("recuadro3").style.display = "block";
+}
+
+
+
+
