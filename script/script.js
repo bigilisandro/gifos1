9b3b81696e77ce56f2d03bbf726652250bfb07c6
Lisandro Bigi <55090798+bigilisandro@users.noreply.github.com>
2020-04-08 16:42:28 -0300

    dasdas

--- script/script.js
+++ script/script.js
@@ -43,37 +43,48 @@ var giphyAPItrend = 'https://api.giphy.com/v1/gifs/trending?api_key=zLngW6Npv5ek
 
         for(i = 0; i < json.data.length; i++) {
             
-            console.log(json.data[i].images.original.url);
+            console.log(json);
 
                 document.getElementById("gif1").src = json.data[0].images.original.url;
+                document.getElementById("gif-sugerencias-title1").innerHTML = json.data[0].title;
                 document.getElementById("gif2").src = json.data[1].images.original.url;
+                document.getElementById("gif-sugerencias-title2").innerHTML = json.data[1].title;
                 document.getElementById("gif3").src = json.data[2].images.original.url;
+                document.getElementById("gif-sugerencias-title3").innerHTML = json.data[2].title;
                 document.getElementById("gif4").src = json.data[3].images.original.url;
+                document.getElementById("gif-sugerencias-title4").innerHTML = json.data[3].title;
                 document.getElementById("gif5").src = json.data[4].images.original.url;
+                document.getElementById("gif-sugerencias-tags1").innerHTML = json.data[4].title;
                 document.getElementById("gif6").src = json.data[5].images.original.url;
+                document.getElementById("gif-sugerencias-tags2").innerHTML = json.data[5].title;
                 document.getElementById("gif7").src = json.data[6].images.original.url;
+                document.getElementById("gif-sugerencias-tags3").innerHTML = json.data[6].title;
                 document.getElementById("gif8").src = json.data[7].images.original.url;
+                document.getElementById("gif-sugerencias-tags4").innerHTML = json.data[7].title;
                 document.getElementById("gif9").src = json.data[8].images.original.url;
+                document.getElementById("gif-sugerencias-tags5").innerHTML = json.data[8].title;
                 document.getElementById("gif10").src = json.data[9].images.original.url;
+                document.getElementById("gif-sugerencias-tags6").innerHTML = json.data[9].title;
                 document.getElementById("gif11").src = json.data[10].images.original.url;
+                document.getElementById("gif-sugerencias-tags7").innerHTML = json.data[10].title;
                 document.getElementById("gif12").src = json.data[11].images.original.url;
+                document.getElementById("gif-sugerencias-tags8").innerHTML = json.data[11].title;
                 document.getElementById("gif13").src = json.data[12].images.original.url;
+                document.getElementById("gif-sugerencias-tags9").innerHTML = json.data[12].title;
                 document.getElementById("gif14").src = json.data[13].images.original.url;
+                document.getElementById("gif-sugerencias-tags10").innerHTML = json.data[13].title;
                 document.getElementById("gif15").src = json.data[14].images.original.url;
+                document.getElementById("gif-sugerencias-tags11").innerHTML = json.data[14].title;
                 document.getElementById("gif16").src = json.data[15].images.original.url;
+                document.getElementById("gif-sugerencias-tags12").innerHTML = json.data[15].title;
                 document.getElementById("gif17").src = json.data[16].images.original.url;
+                document.getElementById("gif-sugerencias-tags13").innerHTML = json.data[16].title;
                 document.getElementById("gif18").src = json.data[17].images.original.url;
+                document.getElementById("gif-sugerencias-tags14").innerHTML = json.data[7].title;
                 document.getElementById("gif19").src = json.data[18].images.original.url;
+                document.getElementById("gif-sugerencias-tags15").innerHTML = json.data[18].title;
                 document.getElementById("gif20").src = json.data[19].images.original.url;
-                
-                
-
-            console.log(json.data[i].title);
-
-              document.getElementById("gif-sugerencias-title1").innerHTML = json.data[0].title + '<img src="assets/close.svg" />';
-              document.getElementById("gif-sugerencias-title2").innerHTML = json.data[1].title + '<img src="assets/close.svg" />';
-              document.getElementById("gif-sugerencias-title3").innerHTML = json.data[2].title + '<img src="assets/close.svg" />';
-              document.getElementById("gif-sugerencias-title4").innerHTML = json.data[3].title + '<img src="assets/close.svg" />';
+                document.getElementById("gif-sugerencias-tags16").innerHTML = json.data[19].title;
         }
     })
 
@@ -130,9 +141,8 @@ var giphyAPIrandom = '';
   }).then(data => {
     console.log(data);
     for(i = 0; i < data.data.length; i++) {
-            
-  
 
+          document.getElementById("texto3").innerHTML = "Resultados para: [ " + searchValue + " ]";     
           document.getElementById("search").src = data.data[0].images.original.url;
           document.getElementById("search2").src = data.data[1].images.original.url;
           document.getElementById("search3").src = data.data[2].images.original.url;
@@ -149,6 +159,7 @@ var giphyAPIrandom = '';
           document.getElementById("search14").src = data.data[13].images.original.url;
           document.getElementById("search15").src = data.data[14].images.original.url;
           document.getElementById("search16").src = data.data[15].images.original.url;
+
     }
 
   return data
